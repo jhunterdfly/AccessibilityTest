a1081ce5a0c02342d8b415d7c38175b9b49f6a71
John Hunter <jhunterdfly@msn.com>
2020-08-20 16:27:25 -0500

    Created AccessibilityTest solution to reproduce the NavigationPage TitleView issue (#11866)
    https://github.com/xamarin/Xamarin.Forms/issues/11866

---
+++ AccessibilityTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AccessibilityTest.Models;
+
+namespace AccessibilityTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ AccessibilityTest/Views/AboutPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AccessibilityTest.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            titleLabel.Focus();
+        }
+    }
+}
